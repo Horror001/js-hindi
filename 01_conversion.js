0f57e4d3c973145352f41cab1b97e7a40b3565b0
Horror001 <priyanshsharma706@gmail.com>
2025-02-09 10:56:16 +0000

    conversion of data types

---
+++ 01_conversion.js
@@ -0,0 +1,25 @@
+// let score = 33  // here the value is in numeric form
+/*let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));      // both represent the same thing. The output would be same.
+
+// the output recieved is number, NOTE  where n is small.  For key word names we use first letter capital for eg . = "Nummber"
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)*/
+
+/*let isLoggedIn = 0
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn);*/
+
+// When converted to boolean , if value is 0 it prints "false" , if value is 1 it prints "true"
+
+let isLoggedIn = "priyansh"
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn);
+
+// When input is "String" and is converted to boolean it prints "True"
+// When input is "" (empty) and is converted to boolean it prints "false"
